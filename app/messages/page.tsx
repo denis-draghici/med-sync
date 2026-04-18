de56de2dc9185e1492adca37c35234c10e9cd60f
Denis <draghicidenis@yahoo.com>
2026-04-18 19:01:31 +0300

    githubAction

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -5,34 +5,35 @@ import { createClient } from "@/utils/supabase/server";
 import MessageDashboard from "./MessageDashboard";
 
 export default async function MessagesPage() {
-    const supabase = await createClient();
-    const {
-        data: { user },
-    } = await supabase.auth.getUser();
-    if (!user?.email) {
-        return <div>Please log in to view your profile.</div>;
-    }
-    const userInfo = await getUserInfo(user.email);
-    if (!userInfo) {
-        return <div>Please log in to view your profile.</div>;
-    }
-    const id = userInfo.role === "DOCTOR" ? userInfo.doctor?.id : userInfo.patient?.id;
-    if (!id) {
-        return <div>Please log in to view your profile.</div>;
-    }
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user?.email) {
+    return <div>Please log in to view your profile.</div>;
+  }
+  const userInfo = await getUserInfo(user.email);
+  if (!userInfo) {
+    return <div>Please log in to view your profile.</div>;
+  }
+  const id =
+    userInfo.role === "DOCTOR" ? userInfo.doctor?.id : userInfo.patient?.id;
+  if (!id) {
+    return <div>Please log in to view your profile.</div>;
+  }
 
-    if (userInfo.role === "PATIENT") {
-        return (
-            <>
-                <Navbar />
-                <MessageDashboard userInfo={userInfo} />
-            </>
-        );
-    }
-
+  if (userInfo.role === "PATIENT") {
     return (
-        <Layout withFooter={false}>
-            <MessageDashboard userInfo={userInfo} />
-        </Layout>
+      <>
+        <Navbar />
+        <MessageDashboard userInfo={userInfo} />
+      </>
     );
+  }
+
+  return (
+    <Layout withFooter={false}>
+      <MessageDashboard userInfo={userInfo} />
+    </Layout>
+  );
 }
